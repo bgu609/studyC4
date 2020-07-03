5fdc1ff8b97a03a84999ca377f22c8431f7eac96
bgu609 <giung609@naver.com>
2020-07-03 12:55:53 +0900

    오전 수업

--- Study_C4/HelloApp/Program.cs
+++ Study_C4/HelloApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace HelloApp
 {
@@ -10,6 +7,13 @@ namespace HelloApp
     {
         static void Main(string[] args)
         {
+            if(args.Length == 0)
+            {
+                WriteLine("ex: HelloApp.exe <이름>");
+                return;
+            }
+
+            WriteLine($"Hello, {args[0]}!");
         }
     }
 }
