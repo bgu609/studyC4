4a4696e012ed7a3c36717f9e7ee0246a5c8da350
bgu609 <giung609@naver.com>
2020-07-06 09:49:53 +0900

    2020-07-06

---
+++ Study_C4/enumApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumApp
+{
+    class Program
+    {
+        enum DialogResult
+        {
+            YES = 10,
+            NO = 20,
+            CANCEL = 30,
+            CONFIRM = 40,
+            OK = 50
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DialogResult.OK);
+            //Console.WriteLine((int)DialogResult.OK);
+            DialogResult result = DialogResult.YES;
+            if (result == DialogResult.YES)
+            {
+                Console.WriteLine("YES를 선택했습니다.");
+            }
+        }
+    }
+}
