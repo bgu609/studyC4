7078dfb81c22a195573b7aade2f0c04da334cc91
bgu609 <giung609@naver.com>
2020-09-03 16:55:35 +0900

    누락되어 있던 자료

---
+++ ChartTestApp/ChartForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ChartTestApp
+{
+    partial class ChartForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChartForm));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.btnDBbind = new MetroFramework.Controls.MetroButton();
+            this.btnMultichart = new MetroFramework.Controls.MetroButton();
+            this.btnDataBind = new MetroFramework.Controls.MetroButton();
+            this.btnXYvalues = new MetroFramework.Controls.MetroButton();
+            this.btnYvalues = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(23, 63);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "score";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(754, 594);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.btnDBbind);
+            this.metroPanel1.Controls.Add(this.btnMultichart);
+            this.metroPanel1.Controls.Add(this.btnDataBind);
+            this.metroPanel1.Controls.Add(this.btnXYvalues);
+            this.metroPanel1.Controls.Add(this.btnYvalues);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(784, 63);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(273, 594);
+            this.metroPanel1.TabIndex = 1;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // btnDBbind
+            // 
+            this.btnDBbind.Location = new System.Drawing.Point(64, 329);
+            this.btnDBbind.Name = "btnDBbind";
+            this.btnDBbind.Size = new System.Drawing.Size(149, 63);
+            this.btnDBbind.TabIndex = 2;
+            this.btnDBbind.Text = "DB bind";
+            this.btnDBbind.UseSelectable = true;
+            this.btnDBbind.Click += new System.EventHandler(this.btnDBbind_Click);
+            // 
+            // btnMultichart
+            // 
+            this.btnMultichart.Location = new System.Drawing.Point(64, 241);
+            this.btnMultichart.Name = "btnMultichart";
+            this.btnMultichart.Size = new System.Drawing.Size(149, 63);
+            this.btnMultichart.TabIndex = 2;
+            this.btnMultichart.Text = "Multi Chart";
+            this.btnMultichart.UseSelectable = true;
+            this.btnMultichart.Click += new System.EventHandler(this.btnMultichart_Click);
+            // 
+            // btnDataBind
+            // 
+            this.btnDataBind.Location = new System.Drawing.Point(64, 172);
+            this.btnDataBind.Name = "btnDataBind";
+            this.btnDataBind.Size = new System.Drawing.Size(149, 63);
+            this.btnDataBind.TabIndex = 2;
+            this.btnDataBind.Text = "Data Bind";
+            this.btnDataBind.UseSelectable = true;
+            this.btnDataBind.Click += new System.EventHandler(this.btnDataBind_Click);
+            // 
+            // btnXYvalues
+            // 
+            this.btnXYvalues.Location = new System.Drawing.Point(64, 72);
+            this.btnXYvalues.Name = "btnXYvalues";
+            this.btnXYvalues.Size = new System.Drawing.Size(149, 63);
+            this.btnXYvalues.TabIndex = 2;
+            this.btnXYvalues.Text = "XY values";
+            this.btnXYvalues.UseSelectable = true;
+            this.btnXYvalues.Click += new System.EventHandler(this.btnXYvalues_Click);
+            // 
+            // btnYvalues
+            // 
+            this.btnYvalues.Location = new System.Drawing.Point(64, 3);
+            this.btnYvalues.Name = "btnYvalues";
+            this.btnYvalues.Size = new System.Drawing.Size(149, 63);
+            this.btnYvalues.TabIndex = 2;
+            this.btnYvalues.Text = "Only Y values";
+            this.btnYvalues.UseSelectable = true;
+            this.btnYvalues.Click += new System.EventHandler(this.btnYvalues_Click);
+            // 
+            // ChartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 680);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.chart1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "ChartForm";
+            this.Resizable = false;
+            this.Text = "ChartTestApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.metroPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroButton btnYvalues;
+        private MetroFramework.Controls.MetroButton btnXYvalues;
+        private MetroFramework.Controls.MetroButton btnDataBind;
+        private MetroFramework.Controls.MetroButton btnMultichart;
+        private MetroFramework.Controls.MetroButton btnDBbind;
+    }
+}
+
---
+++ ChartTestApp/ChartForm.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using MetroFramework.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ChartTestApp
+{
+    public partial class ChartForm : MetroForm
+    {
+        public readonly string strConstring =
+            "Data Source=localhost;" +
+            "Port=3306;" +
+            "Database=employees;" +
+            "Uid=root;" +
+            "Password=epfls+358471;";
+
+        public readonly string strQuery =
+            "SELECT concat(em.first_name, ' ', em.last_name) as emp_name, " +
+            "       em.last_name, " +
+            "       em.gender, " +
+            "       em.hire_date, " +
+            "       dp.dept_no, " +
+            "       dt.dept_name, " +
+	        "       MAX(sl.salary) AS max_salary " +
+            "  FROM employees AS em " +
+            " INNER JOIN dept_emp AS dp " +
+            "    ON em.emp_no = dp.emp_no " +
+            " INNER JOIN departments AS dt " +
+            "    ON dp.dept_no = dt.dept_no " +
+            " INNER JOIN salaries AS sl " +
+            "    ON em.emp_no = sl.emp_no " +
+            " WHERE em.hire_date >= '2000-01-01' " +
+            " GROUP BY em.first_name, em.last_name, em.gender, em.hire_date, dt.dept_name " +
+            " ORDER BY em.first_name ASC; ";
+
+        private class Student
+        {
+            public string Name { get; set; }
+            public double Score { get; set; }
+
+            public Student(string name, double score)
+            {
+                Name = name;
+                Score = score;
+            }
+        }
+
+        public ChartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnYvalues_Click(object sender, EventArgs e)
+        {
+            chartreset();
+
+            //chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.Add(98);
+            chart1.Series[0].Points.Add(72);
+            chart1.Series[0].Points.Add(50);
+            chart1.Series[0].Points.Add(100);
+            chart1.Series[0].Points.Add(88);
+            chart1.Series[0].Points.Add(63);
+
+            chart1.Series[0].IsValueShownAsLabel = true;
+            chart1.Series[0].ChartType = SeriesChartType.Area;
+        }
+
+        private void btnXYvalues_Click(object sender, EventArgs e)
+        {
+            chartreset();
+
+            //chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY("성명건", 98);
+            chart1.Series[0].Points.AddXY("김희지", 72);
+            chart1.Series[0].Points.AddXY("오동욱", 50);
+            chart1.Series[0].Points.AddXY("김민수", 100);
+            chart1.Series[0].Points.AddXY("여예지", 88);
+            chart1.Series[0].Points.AddXY("이채은", 63);
+
+            chart1.Series[0].IsValueShownAsLabel = true;
+            chart1.Series[0].ChartType = SeriesChartType.Pie;
+        }
+
+        private void btnDataBind_Click(object sender, EventArgs e)
+        {
+            chartreset();
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student("성명건", 98.4));
+            students.Add(new Student("이채은", 63.9));
+            students.Add(new Student("여예지", 88.2));
+            students.Add(new Student("김민수", 100.0));
+            students.Add(new Student("오동욱", 50.4));
+            students.Add(new Student("김희지", 72.8));
+
+            chart1.Series[0].Points.DataBind(students, "Name", "Score", null);
+            chart1.Series[0].ChartType = SeriesChartType.Bar;
+            chart1.Series[0].IsValueShownAsLabel = true;
+            chart1.Series[0].Color = Color.BlueViolet;
+        }
+
+        private void btnMultichart_Click(object sender, EventArgs e)
+        {
+            chartreset();
+                        
+            chart1.ChartAreas.Add("area2");
+            //chart1.ChartAreas.Add("area3");
+
+            chart1.Series.Add("series2");
+            chart1.Series.Add("series3");
+
+            chart1.Series["series2"].ChartArea = "area2";
+            chart1.Series["series3"].ChartArea = "area2";
+
+            chart1.Series["series1"].ChartType = SeriesChartType.Spline;
+            chart1.Series["series2"].ChartType = SeriesChartType.Line;
+            chart1.Series["series3"].ChartType = SeriesChartType.Bubble;
+
+            chart1.Series["series1"].Color = Color.Red;
+            chart1.Series["series2"].Color = Color.Blue;
+            chart1.Series["series3"].Color = Color.Green;
+
+            chart1.Series["series1"].Points.AddXY(1, 100);
+            chart1.Series["series1"].Points.AddXY(2, 400);
+            chart1.Series["series1"].Points.AddXY(3, 200);
+            chart1.Series["series1"].Points.AddXY(4, 300);
+            chart1.Series["series1"].Points.AddXY(5, 900);
+
+            chart1.Series["series2"].Points.AddXY(1, 120);
+            chart1.Series["series2"].Points.AddXY(2, 420);
+            chart1.Series["series2"].Points.AddXY(3, 290);
+            chart1.Series["series2"].Points.AddXY(4, 390);
+            chart1.Series["series2"].Points.AddXY(5, 901);
+
+            chart1.Series["series3"].Points.AddXY(1, 220);
+            chart1.Series["series3"].Points.AddXY(2, 330);
+            chart1.Series["series3"].Points.AddXY(3, 660);
+            chart1.Series["series3"].Points.AddXY(4, 440);
+            chart1.Series["series3"].Points.AddXY(5, 550);
+
+            chart1.Series["series2"].Points[3].Color = Color.HotPink;
+            chart1.Series["series2"].Points[4].Color = Color.Orange;
+        }
+
+        private void btnDBbind_Click(object sender, EventArgs e)
+        {
+            chartreset();
+
+            using (MySqlConnection con = new MySqlConnection(strConstring))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(strQuery, con);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+
+                chart1.DataSource = ds.Tables[0];
+                chart1.Series[0].XValueMember = "emp_Name";
+                chart1.Series[0].YValueMembers = "max_salary";
+                chart1.DataBind();
+
+                chart1.ChartAreas[0].AxisX.LabelStyle.Interval = 1;
+                chart1.ChartAreas[0].AxisX.Title = "Employees";
+                chart1.ChartAreas[0].AxisY.Title = "Salaries";
+                chart1.ChartAreas[0].AxisY.LabelStyle.Format = "${0,0}";
+
+                chart1.Series[0].IsValueShownAsLabel = true;
+                chart1.Series[0].Color = Color.DeepPink;
+                chart1.Series[0].Name = "Salary";
+                chart1.Series[0].LabelFormat = "{0,0}";
+            }
+        }
+
+        private void chartreset()
+        {
+            chart1.ChartAreas.Clear();
+            chart1.Series.Clear();
+            chart1.ChartAreas.Add("area1");
+            chart1.Series.Add("series1");
+            chart1.Series["series1"].ChartArea = "area1";
+        }
+    }
+}
